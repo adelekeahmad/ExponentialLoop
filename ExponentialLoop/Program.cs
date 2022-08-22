19c222d4d195aab21eb7b21981fd2c666f8c33a2
Ahmad Bakare <65125857+adelekeahmad@users.noreply.github.com>
2022-08-22 03:38:58 -0700

    Add project files.

---
+++ ExponentialLoop/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace exponetialLoop;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(GetPow(3, 5));
+    }
+    static int GetPow(int baseNum, int powNum)
+    {
+        int result = 1;
+        for(int i = 0; i < powNum; i++)
+        {
+            result = result * baseNum;
+        }
+        return result;
+    }
+}
